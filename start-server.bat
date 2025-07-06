8f98f142433e388be38b66dbff90ddb98ca94bc4
Enrique Walter Philippeaux <enriqueph@gmail.com>
2025-07-05 23:34:45 -0300

    remove old scripts

--- start-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Requisador de Requisitos...
-echo.
-echo Open your browser and go to: http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-cd src
-python -m http.server 8000
